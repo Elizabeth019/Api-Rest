77ae4d107942598e52a83a53a95fe387d619ebae
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-10-18 21:38:27 -0300

    agregando rutas

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 
+const usersRoute= require("./usersRoute.js");
+const productsRoute= require("./productsRoute.js");
 
 const router = Router();
 
-router.get("/",)
+router.use("/user", usersRoute);
+router.use("/products", productsRoute);
 
 module.exports = Router();
