ae4a05433f1d982de93d1a5ea5b9458d711ade6b
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-10-20 21:47:58 -0300

    modificación en las rutas

--- Server/src/routes/usersRoute.js
+++ Server/src/routes/usersRoute.js
@@ -1,4 +1,5 @@
 const {Router}= require('express');
+const { newUser}  = require('../handlers/users');
 const user = Router();
 
 // const {userId, }
@@ -9,8 +10,17 @@ user.get("/", (req, res) => {
   res.send("Hello user");
 });
 
-user.post("/", (res, req) => {
-  res.send("Has creado un usuario");
+user.post("/", async(req, res) => {
+  //console.log("Has creado un usuario");
+  const {name, email, password, typeUser} = req.body;
+  
+  try{
+    const user  = await newUser(name, email, password, typeUser);
+    console.log("nuevo usuario", name);
+    res.status(200).send("Has creado un usuario");
+  }catch(error){
+    res.status(400).json({message: error.message});
+  }
 })
 
 module.exports = user;
