fac483f8ff3863d1b187bfd024df63fb0763b924
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2024-02-23 23:26:48 -0300

    Db.json integrado

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from 'express';
 import fs from 'fs';
-
+import bodyParser from 'body-parser';
 
 
 const app = express();
+app.use(bodyParser.json());
+
+const readData = () =>{
+ try {
+  const data =fs.readFileSync('./db.json');
+  return JSON.parse(data);
+ } catch (error) {
+  console.log(error);
+ }
+}
 
 app.get('/', (req, res)=>{
   res.send('Mi Api con Nodejs')
 });
 
+app.get('/usuarios', (req, res)=>{
+  const users= readData();
+   res.json(users);
+})
 
 
 app.listen(3000, ()=>{
   console.log("Servidor corriendo en el puerto 3000")
-})
+});
